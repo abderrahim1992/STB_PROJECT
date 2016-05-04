39a021327d16702262cdce5f1d7cf569dd6d7ab2
abderrahim1992 <rhm.siziani@gmail.com>
2016-05-04 22:03:49 +0200

    exigence multiple rectification

--- src/main/java/stb/model/StbClient.java
+++ src/main/java/stb/model/StbClient.java
@@ -12,7 +12,6 @@ public class StbClient {
 	 * @param args
 	 */
 	@XmlElement
-<<<<<<< HEAD
     private String nomClient;
     
     @XmlElement
@@ -78,110 +77,4 @@ public class StbClient {
     public void setAdresse(Adresse adresse){
     	this.adresse=adresse;
     }  
-=======
-	private String nomClient;
-
-	@XmlElement
-	private String prenomClient;
-
-	@XmlElement
-	private String genderClient;
-
-	@XmlElement
-	private String contactClient;
-
-	@XmlElement
-	private String numRue;
-
-	@XmlElement
-	private String nomRue;
-
-	@XmlElement
-	private String nomVille;
-
-	@XmlElement
-	private String codePostal;
-
-	public StbClient(String nom, String prenom, String gender, String contact, String num, String rue, String ville,
-			String code) {
-		this.nomClient = nom;
-		this.prenomClient = prenom;
-		this.genderClient = gender;
-		this.contactClient = contact;
-		this.numRue = num;
-		this.nomRue = rue;
-		this.nomVille = ville;
-		this.codePostal = code;
-	}
-
-	public StbClient() {
-	}
-
-	// getters
-	public String getNomClient() {
-		return nomClient;
-	}
-
-	public String getPrenomClient() {
-		return prenomClient;
-	}
-
-	public String getGenderClient() {
-		return genderClient;
-	}
-
-	public String getContactClient() {
-		return contactClient;
-	}
-
-	public String getnumRue() {
-		return numRue;
-	}
-
-	public String getNomRue() {
-		return nomRue;
-	}
-
-	public String getNomVille() {
-		return nomVille;
-	}
-
-	public String getCode() {
-		return codePostal;
-	}
-
-	// setters
-	public void setNomClient(String nomClient) {
-		this.nomClient = nomClient;
-	}
-
-	public void setPrenomClient(String prenomClient) {
-		this.prenomClient = prenomClient;
-	}
-
-	public void setGenderClient(String genderClient) {
-		this.genderClient = genderClient;
-	}
-
-	public void setContactClient(String contactClient) {
-		this.contactClient = contactClient;
-	}
-
-	public void setnumRue(String numRue) {
-		this.numRue = numRue;
-	}
-
-	public void setNomRue(String nomRue) {
-		this.nomRue = nomRue;
-	}
-
-	public void setNomVille(String nomVille) {
-		this.nomVille = nomVille;
-	}
-
-	public void setCode(String codePostal) {
-		this.codePostal = codePostal;
-	}
-
->>>>>>> 8357dc952fd098fe795d81f96fbbeb20cabcd5fb
 }
